7b7943e96f2811084f6976b7f0b39bf4518e9a47
Caspar <casparwilson111@gmail.com>
2024-07-31 15:21:00 +0100

    KAN-31 added static html elements to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+// import { DemonstratingProps } from "./components/demo/DemonstratingProps";
 
 function App() {
     return (
         <div>
-            <h1>App component</h1>
-            <DemonstratingProps />
+            <h1>BCP - OMDB Movie Explorer</h1>
+            <div>
+                <h4>Search movies</h4>
+                <input></input>
+                <button>Search</button>
+            </div>
+            <div className="comments_section">
+                <h3>Our comments on movies</h3>
+                <button>Refresh</button>
+            </div>
         </div>
     );
 }
